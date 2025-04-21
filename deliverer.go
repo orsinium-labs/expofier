7f670f81c9ca9b5bd0d8e303d1bca92b8490d284
gram <git@orsinium.dev>
2025-04-21 14:00:11 +0200

    improve Promise API

--- deliverer.go
+++ deliverer.go
@@ -6,13 +6,37 @@ import (
 )
 
 type Promise struct {
-	Done  bool
-	Error error
+	done chan struct{}
+	err  error
+	cb   func(error)
 }
 
+// Channel that will be closed when the Promise is done.
+func (p *Promise) Done() <-chan struct{} {
+	return p.done
+}
+
+func (p *Promise) Err() error {
+	select {
+	case <-p.done:
+	default:
+		panic("trying to get Err before Promise is Done")
+	}
+	return p.err
+}
+
+// Register function to be called when Promise is Done.
+func (p *Promise) Callback(cb func(error)) {
+	p.cb = cb
+}
+
+// Set the error (if not nil) and mark the Promise as Done.
 func (p *Promise) Resolve(err error) {
-	p.Error = err
-	p.Done = true
+	p.err = err
+	close(p.done)
+	if p.cb != nil {
+		p.cb(err)
+	}
 }
 
 type sendJob struct {
